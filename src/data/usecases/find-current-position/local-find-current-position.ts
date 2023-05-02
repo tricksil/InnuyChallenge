3caad705f4d53c77fcf4e843da4e82d97f855666
Patrick Silva <patrickeeep12@gmail.com>
2023-05-02 10:59:35 -0300

    feat: ensure LocalFindCurrentPosition throws PermissionDeniedErro when dont have permission

--- src/data/usecases/find-current-position/local-find-current-position.ts
+++ src/data/usecases/find-current-position/local-find-current-position.ts
@@ -1,10 +1,15 @@
 import { GetLocation } from '@/data/protocols';
+import { PermissionDenied } from '@/domain/error/permission-danied';
 import { LocationModel } from '@/domain/models';
 import { FindCurrentPosition } from '@/domain/usecases';
 
 export class LocalFindCurrentPosition implements FindCurrentPosition {
   constructor(private readonly getLocation: GetLocation) {}
   async coords(): Promise<LocationModel> {
-    return await this.getLocation.get();
+    try {
+      return await this.getLocation.get();
+    } catch (error) {
+      throw new PermissionDenied();
+    }
   }
 }
