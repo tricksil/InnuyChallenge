9e154b3c388bd39513f30aeed3699c3a6c529c1b
Patrick Silva <patrickeeep12@gmail.com>
2023-05-02 10:21:36 -0300

    chore: update baseUrl project and app config

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+const config = getDefaultConfig(__dirname);
+config.transformer = {
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
+module.exports = config;
